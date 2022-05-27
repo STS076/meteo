5793a51c568c02dd977123086eb5926cb4e656f2
STS076 <sophfrom76@hotmail.com>
2022-05-27 22:33:35 +0200

    script + style

--- script/script.js
+++ script/script.js
@@ -4,7 +4,6 @@ function frenchDate(longDate) {
     return arrayDate[2] + ' ' + arrayMonths[+arrayDate[1] - 1] + ' ' + arrayDate[0] 
 }
 
-
 function frenchHour(longDate) { 
     let arrayHour = longDate.split(' ')[1].split(':') 
     return arrayHour[0] + 'h00' 
@@ -21,6 +20,7 @@ function createForecast(data, totalPreview) {
             <p class="main-hour mb-0">${frenchHour(data.list[0].dt_txt)}</p>
             <img src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png" alt="${data.list[0].weather[0].description}">
             <p class="main-temp mb-0">${data.list[0].main.temp}°C</p>
+            <p class=" mb-0"><i class="bi bi-wind"></i> ${data['list'][0]['wind']['speed']} km/h </p>
         </div>
     `)
 
@@ -31,8 +31,9 @@ function createForecast(data, totalPreview) {
             <p class="fs-6 mb-0 mt-1">${frenchDate(data.list[i].dt_txt)}</p>
             <p class="fs-5 mb-0">${frenchHour(data.list[i].dt_txt)}</p>
             <img src="http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="${data.list[i].weather[0].description}">
-            <p class="fs-5 mb-0">${data.list[i].weather[0].description}</p>
-            <p class="fs-3 fw-bold">${data.list[i].main.temp}°C</p>
+            <p class=" mb-0">${data.list[i].weather[0].description}</p>
+            <p class=" fw-bold">${data.list[i].main.temp}°C</p>
+            <p class=" mb-0"><i class="bi bi-wind"></i> ${data['list'][0]['wind']['speed']} km/h </p>
         </div>
     `)
     }
@@ -40,11 +41,12 @@ function createForecast(data, totalPreview) {
     let tableForecast = document.getElementById('tableForecast')
     for (let i = 1; i <= totalPreview; i++) {
         tableForecast.insertAdjacentHTML('beforeend', `
-        <tr class="align-middle pt-5 ">
+        <tr class="align-middle pt-5 text-dark ">
             <td>${frenchHour(data.list[i].dt_txt)}</td>
             <td>${data.list[i].weather[0].description}</td>
             <td><img src="http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png" alt="${data.list[i].weather[0].description}"></td>
             <td>${data.list[i].main.temp}°C</td>
+            <td class=" mb-0"><i class="bi bi-wind"></i> ${data['list'][0]['wind']['speed']} km/h </td>
         </tr>
         `)
     }
@@ -60,7 +62,6 @@ function displayMeteo(id) {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${apiKey}&lang=${lang}&units=${metric}`)
         .then(reponse => reponse.json())
         .then(data => {
-            console.log(data)
 
             createForecast(data, 5)
         })
@@ -71,10 +72,11 @@ function displayMeteo(id) {
 //  2643743 = Londres
 //  5391811 = San Diego
 
-displayMeteo(6456451)
+// displayMeteo(6456451)
 
 function showForecast(id) {
     let landingPage = document.getElementById('landingPage')
+    let favoris = document.getElementById('favoris')
     let mainForecast = document.getElementById('mainForecast')
     mainForecast.innerHTML = ''
     let multiForecast = document.getElementById('multiForecast')
@@ -82,6 +84,7 @@ function showForecast(id) {
     let tableForecast = document.getElementById('tableForecast')
     tableForecast.innerHTML = ''
     landingPage.style.display = 'none'
+    favoris.style.display = 'block'
     displayMeteo(id)
 
 }
